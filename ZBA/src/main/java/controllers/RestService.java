7ff40b1fc273725050e947bf9a41139828f5bfb0
ianthazlett <56487591+ianthazlett@users.noreply.github.com>
2020-02-09 21:08:06 -0500

    Add files via upload

--- ZBA/src/main/java/controllers/RestService.java
+++ ZBA/src/main/java/controllers/RestService.java
@@ -2,27 +2,30 @@ package controllers;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import models.CircleZone;
+import models.UserModel;
 
-@Service
-public class RestService {
+@RestController
+class RestService {
 
     private final RestTemplate restTemplate;
 
     public RestService(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
-
-    public String getPostsPlainJSON() {
-        String url = "http://localhost:8080/Login";
-        return this.restTemplate.getForObject(url, String.class);
-    }
     
-	public CircleZone[] getCircleZoneAsObject() {
-	    String url = "http://localhost:8080/Login";
-	    return this.restTemplate.getForObject(url, CircleZone[].class);
-	}
-	
+    @PostMapping("/Login")
+    UserModel user(@RequestParam String Email, @RequestParam String Password) {
+    	System.out.print("Made it");
+    	UserModel temp = null;
+    	return temp;
+    }
 }
